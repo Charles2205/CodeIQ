9a12b26c30a2fbbfa7f7f7b6c023986025ad4fb1
Charles2205 <cewudzie0@gmail.com>
2023-07-19 09:23:54 +0000

    Fetched Questions and Answers && Updated HomePae

--- public/js/testingQuestions.js
+++ public/js/testingQuestions.js
@@ -1,36 +1,62 @@
-<h2 class="my-5">What tag is used to insert image into your webPage</h2>
+// fetching data from the api endpoint
+const API_KEY ='vKI5iIXDiOGE7eVu9K6mDGiMaXKZwGvD9UKk6JiR'
+const quizQuestions = async()=>{
+    return await axios.get(`https://quizapi.io/api/v1/questions?apiKey=${API_KEY}&category=code&difficulty=Easy&limit=1&tags=HTML`)
+} 
+
+
+
+// generate Questions
+const questionTag = document.querySelector('#question-tag')
+const questions = async () => {
+    let {data}= await quizQuestions()
+    data=(data[0])
+    let answers =data.answers
+    console.log(answers)
+    console.log(data);
+    questionTag.innerHTML=`
+    <h2 class="my-5">${data.question}</h2>
         <div class="form-check fs-3">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
             <label class="form-check-label" for="flexRadioDefault1">
-              H1 Tag
+              ${answers.answer_a}
             </label>
           </div>
 
           <div class="form-check fs-3">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
             <label class="form-check-label" for="flexRadioDefault2">
-              Span Tag
+            ${answers.answer_b}
             </label>
           </div>
 
           <div class="form-check fs-3">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" checked>
             <label class="form-check-label" for="flexRadioDefault3">
-             Div Tag 
+            ${answers.answer_c} 
             </label>
           </div>
 
           <div class="form-check fs-3">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4" checked>
             <label class="form-check-label" for="flexRadioDefault4">
-              Label
+            ${answers.answer_d}
             </label>
           </div>
 
           <div class="form-check fs-3">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault5" checked>
             <label class="form-check-label" for="flexRadioDefault5">
-              Select Tag
+            ${answers.answer_e}
             </label>
           </div>
         </div>
+    
+    
+    
+    
+    `
+    console.log(questionTag);
+    
+}
+questions()
